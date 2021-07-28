3ed626fd6e09c2b5e644acd651d799598f56deb0
Jordan Read <jread6477@gmail.com>
2021-07-27 21:20:21 -0400

    JsonChallenge

---
+++ JSONChallenge/Program.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.FirstName = "Jordan";
+            person.LastName = "Read";
+            person.DOB = new DateTime(1989, 09, 15);
+            person.FavoriteIceCreamFlavors = new List<string> { "Strawberry", "Cookie Dough" };
+
+            string json = JsonConvert.SerializeObject(person);
+
+            Console.WriteLine(json);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ JSONChallenge/Receipt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONChallenge
+{
+    public class Receipt
+    {
+        public string OrderId { get; set; }
+        public Customer Customer { get; set; }
+        public List<LineItems> LineItems { get; set; }
+        public DateTime PurchaseDate { get; set; }
+    }
+}
